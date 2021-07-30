fe3d39118b4e56e616b8507c63b3ed854af6325a
Dan <haidangtu2001@gmail.com>
2021-07-30 13:03:56 +0200

    Test2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.get('/', (req, res) => {
-    res.send('TEST GITHUB DEPLOY');
+    res.send('TEST GITHUB DEPLOY 2');
 });
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
