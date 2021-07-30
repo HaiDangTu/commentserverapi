6767582f89a183c2ea8833edfffd9ba03e601a78
HaiDangTu <59450656+HaiDangTu@users.noreply.github.com>
2021-07-30 12:57:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.get('/', (req, res) => {
-    res.send('Hello World, from express');
+    res.send('TEST GITHUB DEPLOY');
 });
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
